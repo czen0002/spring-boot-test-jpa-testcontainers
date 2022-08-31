8be74323f11aeb52ea966889ada94b55dd663671
czen0002 <zc114789@gmail.com>
2022-08-31 22:32:15 +1000

    [CHENG] init commit

---
+++ src/main/java/io/czen/testjpatestcontainers/TestJpaTestcontainersApplication.java
@@ -0,0 +1,13 @@
+package io.czen.testjpatestcontainers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJpaTestcontainersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestJpaTestcontainersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/czen/testjpatestcontainers/TestJpaTestcontainersApplicationTests.java
@@ -0,0 +1,13 @@
+package io.czen.testjpatestcontainers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestJpaTestcontainersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
